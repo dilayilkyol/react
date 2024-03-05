732aab8a33282e33dea9b16e47c349bb78d42789
dilayilkyol <m.dilaaay@gmail.com>
2024-03-05 21:48:37 +0300

    third commit

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -7,6 +7,8 @@ import {
   Routes, 
   Route
 } from "react-router-dom";
+import "./App.css"
+
 
 function App() {
   
@@ -22,6 +24,34 @@ function App() {
         </Routes>
       </Router>
     </div>
+
+    <div className="edge">
+      <header>
+
+      <div>
+      <img src="/images/food1.jpeg" alt="" class="center"/>
+    </div>
+
+    <div>
+      <div className="title">
+      <h2>Site Name</h2>
+    </div>
+
+    <span className="location">
+      8370 N 4th St, Los Angeles, CA 59930, USA
+    </span>
+
+    <span className="location">
+    Open Hours: 8.00 AM - 8.00 PM
+    </span>
+    </div>
+
+    
+
+      </header>
+    </div>
+    
+
 
     </>
   )
