9f7db27729c9f1d3f0fdfcf1de08bd3d77b37766
dilayilkyol <m.dilaaay@gmail.com>
2024-03-16 12:24:45 +0300

    forth commit

--- front-end/src/pages/Home.js
+++ front-end/src/pages/Home.js
@@ -1,3 +1,314 @@
+import "../styles.css"
+import React from 'react';
+
+
 export default function Home() {
-    return <h1></h1>
+
+
+  return (
+    <>
+
+      <div className="edge">
+        <header>
+
+          <div>
+            <img src="/images/main-img.jpeg" alt="" class="center" />
+          </div>
+
+          <div className="info">
+
+            <div className="title">
+              <h2>Site Name</h2>
+            </div>
+
+            <span className="location">
+              8370 N 4th St, Los Angeles, CA 59930, USA
+            </span>
+
+            <span className="location">
+              Open Hours: 8.00 AM - 8.00 PM
+            </span>
+
+          </div>
+
+        </header>
+      </div>
+
+      <br></br>
+      <br></br>
+
+      <span className="discover">
+        Discover new tastes
+      </span>
+
+      <div class="container content">
+        <div class="row firstRow">
+          <div class="col-md-3">
+            <img src="/contentimg/lunch.jpeg" alt="" className="content-img" />
+            <button className="button">
+              Lunch
+            </button>
+          </div>
+
+          <div class="col-md-3">
+            <img src="/contentimg/dinner.jpeg" alt="" className="content-img" />
+            <button className="button">
+              Dinner
+            </button>
+          </div>
+
+          <div class="col-md-3">
+            <img src="/contentimg/desert.jpeg" alt="" className="content-img" />
+            <button className="button">
+              Desert
+            </button>
+          </div>
+
+          <div class="col-md-3">
+            <img src="/contentimg/drink.jpeg" alt="" className="content-img" />
+            <button className="button">
+              Drinks
+            </button>
+          </div>
+
+        </div>
+      </div>
+
+      <div class="lunch-title">
+        <h3>Lunch</h3>
+      </div>
+
+      <div class="one">
+        <div class="part1">
+
+          <div class="other1">
+          <div class="firstboxDot ">
+            <div class="firstbox">
+              <div data-anchor-id="menuItem" data-item-id="1" class="firstItem">
+                <div aria-label="Chicken Seacer Salad" role="button" tabindex="0" class="saladbox"></div>
+                <div class="sc62-0">
+                  <div data-test-id="GenericItemCard" class="itemCard">
+                    <div class="stackStyle">
+                      <div class="title">
+                        <h3 display="block" data-telemetry-id="storeMenuItem.title" color="TextPrimary"
+                          overflow="truncate" class="titleStyle">Chicken Seacer Salad</h3>
+                      </div>
+                    </div>
+                    <div class="stackStyle">
+                      <span display="block" data-telemetry-id="storeMenuItem.subtitle" color="TextTertiary"
+                        overflow="truncate" class="exStyle">
+                        served on Bub and Grandma's sesame bread, garnished with tomato and radish.</span>
+                    </div>
+                    <div class="inlineChild">
+                      <div class="stackStyle">
+                        <div data-test-id="formatted-dynamic-string" class="inlineChild">
+                          <span class="cost">
+                            <div class="inlineChild-etc">
+                              <span overflow="truncate" display="block" data-anchor-id="StoreMenuItemPrice"
+                                class="costStyle">$15.50</span>
+                            </div>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="sc62-9">
+                  <button shape="Pill" size="12" kind="BUTTON/PLAIN" aria-label="Add item to cart"
+                    data-testid="quick-add-button" type="button" class="buttonStyle">
+                    <span kind="BUTTON/PLAIN" class="insetStyle">
+                      <span class="inlineChildAdd">
+                        <span class="mainContent">
+                          <span overflow="truncate" display="block" class="textElement">
+                            <div class="textDiv">
+                              <span overflow="normal" display="block" class="textElementStyle">Add</span>
+                            </div>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </button>
+                </div>
+                <div class="fd2">
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+
+          <div class="other2">
+          <div class="firstboxDot" >
+          <div class="firstbox">
+              <div data-anchor-id="menuItem" data-item-id="1" class="firstItem">
+                <div aria-label="Chicken Seacer Salad" role="button" tabindex="0" class="saladbox"></div>
+                <div class="sc62-0">
+                  <div data-test-id="GenericItemCard" class="itemCard">
+                    <div class="stackStyle">
+                      <div class="title">
+                        <h3 display="block" data-telemetry-id="storeMenuItem.title" color="TextPrimary"
+                          overflow="truncate" class="titleStyle">Chicken Seacer Salad</h3>
+                      </div>
+                    </div>
+                    <div class="stackStyle">
+                      <span display="block" data-telemetry-id="storeMenuItem.subtitle" color="TextTertiary"
+                        overflow="truncate" class="exStyle">
+                        served on Bub and Grandma's sesame bread, garnished with tomato and radish.</span>
+                    </div>
+                    <div class="inlineChild">
+                      <div class="stackStyle">
+                        <div data-test-id="formatted-dynamic-string" class="inlineChild">
+                          <span class="cost">
+                            <div class="inlineChild-etc">
+                              <span overflow="truncate" display="block" data-anchor-id="StoreMenuItemPrice"
+                                class="costStyle">$15.50</span>
+                            </div>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="sc62-9">
+                  <button shape="Pill" size="12" kind="BUTTON/PLAIN" aria-label="Add item to cart"
+                    data-testid="quick-add-button" type="button" class="buttonStyle">
+                    <span kind="BUTTON/PLAIN" class="insetStyle">
+                      <span class="inlineChildAdd">
+                        <span class="mainContent">
+                          <span overflow="truncate" display="block" class="textElement">
+                            <div class="textDiv">
+                              <span overflow="normal" display="block" class="textElementStyle">Add</span>
+                            </div>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </button>
+                </div>
+                <div class="fd2">
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+
+          <div class="other3">
+          <div class="firstboxDot">
+          <div class="firstbox">
+              <div data-anchor-id="menuItem" data-item-id="1" class="firstItem">
+                <div aria-label="Chicken Seacer Salad" role="button" tabindex="0" class="saladbox"></div>
+                <div class="sc62-0">
+                  <div data-test-id="GenericItemCard" class="itemCard">
+                    <div class="stackStyle">
+                      <div class="title">
+                        <h3 display="block" data-telemetry-id="storeMenuItem.title" color="TextPrimary"
+                          overflow="truncate" class="titleStyle">Chicken Seacer Salad</h3>
+                      </div>
+                    </div>
+                    <div class="stackStyle">
+                      <span display="block" data-telemetry-id="storeMenuItem.subtitle" color="TextTertiary"
+                        overflow="truncate" class="exStyle">
+                        served on Bub and Grandma's sesame bread, garnished with tomato and radish.</span>
+                    </div>
+                    <div class="inlineChild">
+                      <div class="stackStyle">
+                        <div data-test-id="formatted-dynamic-string" class="inlineChild">
+                          <span class="cost">
+                            <div class="inlineChild-etc">
+                              <span overflow="truncate" display="block" data-anchor-id="StoreMenuItemPrice"
+                                class="costStyle">$15.50</span>
+                            </div>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="sc62-9">
+                  <button shape="Pill" size="12" kind="BUTTON/PLAIN" aria-label="Add item to cart"
+                    data-testid="quick-add-button" type="button" class="buttonStyle">
+                    <span kind="BUTTON/PLAIN" class="insetStyle">
+                      <span class="inlineChildAdd">
+                        <span class="mainContent">
+                          <span overflow="truncate" display="block" class="textElement">
+                            <div class="textDiv">
+                              <span overflow="normal" display="block" class="textElementStyle">Add</span>
+                            </div>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </button>
+                </div>
+                <div class="fd2">
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+
+          <div class="other4">
+          <div class="firstboxDot">
+          <div class="firstbox">
+              <div data-anchor-id="menuItem" data-item-id="1" class="firstItem">
+                <div aria-label="Chicken Seacer Salad" role="button" tabindex="0" class="saladbox"></div>
+                <div class="sc62-0">
+                  <div data-test-id="GenericItemCard" class="itemCard">
+                    <div class="stackStyle">
+                      <div class="title">
+                        <h3 display="block" data-telemetry-id="storeMenuItem.title" color="TextPrimary"
+                          overflow="truncate" class="titleStyle">Chicken Seacer Salad</h3>
+                      </div>
+                    </div>
+                    <div class="stackStyle">
+                      <span display="block" data-telemetry-id="storeMenuItem.subtitle" color="TextTertiary"
+                        overflow="truncate" class="exStyle">
+                        served on Bub and Grandma's sesame bread, garnished with tomato and radish.</span>
+                    </div>
+                    <div class="inlineChild">
+                      <div class="stackStyle">
+                        <div data-test-id="formatted-dynamic-string" class="inlineChild">
+                          <span class="cost">
+                            <div class="inlineChild-etc">
+                              <span overflow="truncate" display="block" data-anchor-id="StoreMenuItemPrice"
+                                class="costStyle">$15.50</span>
+                            </div>
+                          </span>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="sc62-9">
+                  <button shape="Pill" size="12" kind="BUTTON/PLAIN" aria-label="Add item to cart"
+                    data-testid="quick-add-button" type="button" class="buttonStyle">
+                    <span kind="BUTTON/PLAIN" class="insetStyle">
+                      <span class="inlineChildAdd">
+                        <span class="mainContent">
+                          <span overflow="truncate" display="block" class="textElement">
+                            <div class="textDiv">
+                              <span overflow="normal" display="block" class="textElementStyle">Add</span>
+                            </div>
+                          </span>
+                        </span>
+                      </span>
+                    </span>
+                  </button>
+                </div>
+                <div class="fd2">
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+          
+        </div>
+      </div>
+
+
+
+
+    </>
+  )
+
 }
+
