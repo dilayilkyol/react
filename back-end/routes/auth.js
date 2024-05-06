caf197261836ab5d58934e9aede3d094e64c6463
Ufuk Mehmetoglu <uerdem@amazon.com>
2024-05-06 13:18:36 -0700

    Removes node modules and updates port

---
+++ back-end/routes/auth.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
+const User = require('../models/User');
+
+router.get('/', async (req, res) => {
+  res.send('Hello World!')
+})
+
+router.post('/signup', async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
+    console.log('req.body',req.body)
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({ username, email, password: hashedPassword });
+    await newUser.save();
+    res.status(201).json(newUser);
+    res.status(200).json({ user: { username: user.username, email: user.email } });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+router.post('/signin', async (req, res) => {
+    try {
+      const { email, password } = req.body;
+      const user = await User.findOne({ email });
+      if (!user) {
+        return res.status(404).json({ message: 'User not found' });
+      }
+      const validPassword = await bcrypt.compare(password, user.password);
+      if (!validPassword) {
+        return res.status(401).json({ message: 'Invalid password' });
+      }
+      res.status(200).json({ message: 'Login successful' });
+    } catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  });
+  
+
+module.exports = router;
---
+++ back-end/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/auth'); // Doğru routes dosyasını import ediyor musunuz?
+
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/auth', userRoutes); // Doğru yol mu?
+
+
+const PORT = process.env.PORT || 4000; // Port tanımı
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
