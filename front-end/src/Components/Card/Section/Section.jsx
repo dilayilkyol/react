1e4ad92c78184d953ad9e06d0e7ce557fedeb0e3
dilayilkyol <m.dilaaay@gmail.com>
2024-05-21 22:27:32 +0300

    products filter mapped

--- front-end/src/Components/Card/Section/Section.jsx
+++ front-end/src/Components/Card/Section/Section.jsx
@@ -1,28 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Card from '../Card';
-import { lunchItems, dinnerItems, dessertItems, drinkItems } from '../../../data/menu.js';
 import "./Section.css";
-
+import axios from 'axios'
 
 const Section = ({ title }) => {
-  let items;
+  const  [products, setProducts] = useState([])
+  useEffect(()=> {
+    
 
-  switch (title) {
-    case 'Lunch':
-      items = lunchItems;
-      break;
-    case 'Dinner':
-      items = dinnerItems;
-      break;
-    case 'Dessert':
-      items = dessertItems;
-      break;
-    case 'Drink':
-      items = drinkItems;
-      break;
-    default:
-      items = [];
-  }
+// Make a request for a user with a given ID
+axios.get('http://localhost:4000/products')
+  .then(function (response) {
+    // handle success
+    
+    setProducts(response.data)
+  })
+  .catch(function (error) {
+    // handle error
+    console.log(error);
+  })
+  .finally(function () {
+    // always executed
+  });
+
+  },[])
+
 
   return (
     <div className="section-container">
@@ -31,7 +33,7 @@ const Section = ({ title }) => {
       </div>
 
       <div className="section-cards">
-        {items.map((item, index) => (
+        {products.filter(product => product.type == title).map((item, index) => (
           <Card
             key={index}
             title={item.title}
